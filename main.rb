a1e641f15375fe5a3f9ae1ad22a9194ea2d0b30b
IAIR <idz420@gmail.com>
2023-02-09 21:16:45 -0300

    Added player class

--- main.rb
+++ main.rb
@@ -1,9 +1,10 @@
 class Board
 
-  attr_accessor :empty_board
+  attr_accessor :empty_board, :game_over
 
   def initialize
     @empty_board = Array(1..10)
+    @game_over = false
   end
 
   def show_board
@@ -19,8 +20,41 @@ class Board
        @empty_board[3..5] == symbol || 
        @empty_board[6..8] == symbol ||
        @empty_board[0] == symbol && @empty_board[4] == symbol && @empty_board[8] == symbol ||
-       @empty_board[6] == symbol && @empty_board[4] == symbol && @empty_board[2] == symbol
+       @empty_board[6] == symbol && @empty_board[4] == symbol && @empty_board[2] == symbol ||
+       @empty_board[0] == symbol && @empty_board[3] == symbol && @empty_board[6] == symbol ||
+       @empty_board[1] == symbol && @empty_board[4] == symbol && @empty_board[7] == symbol ||
+       @empty_board[2] == symbol && @empty_board[5] == symbol && @empty_board[8] == symbol
+        @game_over = true
+        puts "Player #{symbol} won."
+    end
+  end
+
 end
 
-new_board = Board.new
+class Player
+
+  attr_reader :name, :symbol
+
+  def initialize(name, symbol)
+    @name = name
+    @symbol = symbol
+  end
+
+  def make_play(board, move)
+    board[move-1] = @symbol
+  end
+
+end
+
+
 new_board.show_board
+player1 = Player.new("bach", "B")
+player1.make_play(new_board.empty_board, 1)
+player1.make_play(new_board.empty_board, 5)
+player1.make_play(new_board.empty_board, 9)
+new_board.show_board
+new_board.check_winner(player1.symbol)
+
+def play
+  new_board = Board.new
+  players = []
